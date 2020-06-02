a71b13215a81a853f294ac5863fb19990f9a1c80
João Vitor <joao_vitor9846@hotmail.com>
2020-06-02 14:15:45 -0300

    Add /items route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 
+import knex from './database/connection';
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  return res.json({ message: 'Hello NLW' });
+router.get('/items', async (req, res) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map((item) => ({
+    title: item.title,
+    image_url: `http://localhost:3333/uploads/${item.image}`,
+  }));
+
+  return res.json(serializedItems);
 });
 
 export default router;
