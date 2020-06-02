47999d99e65fd927b818f70fd83c04397dea47c1
João Vitor <joao_vitor9846@hotmail.com>
2020-06-02 14:52:14 -0300

    Add route for registering points

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -8,11 +8,49 @@ router.get('/items', async (req, res) => {
   const items = await knex('items').select('*');
 
   const serializedItems = items.map((item) => ({
+    id: item.id,
     title: item.title,
     image_url: `http://localhost:3333/uploads/${item.image}`,
   }));
 
   return res.json(serializedItems);
+});
+
+router.post('/points', async (req, res) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = req.body;
+
+  const trx = await knex.transaction();
+
+  const [point_id] = await trx('points')
+    .insert({
+      image: 'image-fake',
+      name,
+      email,
+      whatsapp,
+      latitude,
+      longitude,
+      city,
+      uf,
+    })
+    .returning('id');
+
+  const pointItems = items.map((item_id: string) => ({
+    item_id,
+    point_id,
+  }));
+
+  await trx('point_items').insert(pointItems);
+
+  return res.status(201).send();
 });
 
 export default router;
