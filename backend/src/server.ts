93cc6049c6ab4e16523f45d553ae3b9a6894b09d
João Vitor <joao_vitor9846@hotmail.com>
2020-06-02 13:24:02 -0300

    Setup static asset access

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,8 +6,11 @@ import express from 'express';
 import cors from 'cors';
 
 import routes from './routes';
+import path from 'path';
 
 const app = express();
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.use(express.json());
 
