5eaa6d07bf88e008fbffc72b2986f5f7dd92ab39
João Vitor <joao_vitor9846@hotmail.com>
2020-06-02 14:59:53 -0300

    Remove route functionalities from route file

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,56 +1,13 @@
 import express from 'express';
 
 import knex from './database/connection';
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 const router = express.Router();
 
-router.get('/items', async (req, res) => {
-  const items = await knex('items').select('*');
+router.get('/items', ItemsController.index);
 
-  const serializedItems = items.map((item) => ({
-    id: item.id,
-    title: item.title,
-    image_url: `http://localhost:3333/uploads/${item.image}`,
-  }));
-
-  return res.json(serializedItems);
-});
-
-router.post('/points', async (req, res) => {
-  const {
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-    items,
-  } = req.body;
-
-  const trx = await knex.transaction();
-
-  const [point_id] = await trx('points')
-    .insert({
-      image: 'image-fake',
-      name,
-      email,
-      whatsapp,
-      latitude,
-      longitude,
-      city,
-      uf,
-    })
-    .returning('id');
-
-  const pointItems = items.map((item_id: string) => ({
-    item_id,
-    point_id,
-  }));
-
-  await trx('point_items').insert(pointItems);
-
-  return res.status(201).send();
-});
+router.post('/points', PointsController.create);
 
 export default router;
