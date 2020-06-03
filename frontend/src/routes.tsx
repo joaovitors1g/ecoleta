3f07290a88a78adf1ef253e3be354a557547e3fa
João Vitor <joao_vitor9846@hotmail.com>
2020-06-03 15:32:07 -0300

    Add Home and Create Point routes

---
+++ frontend/src/routes.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import CreatePoint from './pages/CreatePoint';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/create-point' component={CreatePoint} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
